c0ab6f32bb4835c6d7b057c5c8544512ef459f40
Chillburger <letitbespliced7@gmail.com>
2017-07-03 11:27:27 -0400

    going through type check tutorials

--- src/p.py
+++ src/p.py
@@ -7,12 +7,27 @@ def repeat(s, exclaim):
         result = result + '!!!'
     return result
 
+def absolute(num):
+    try:
+        return abs(int(num))
+    except ValueError:
+        return "the input: " + num + " is not an integer"
+
+def isString(input):
+    callable(test)
+    try:
+        callable(test)
+        return True
+    except StringException:
+        return False
+
+class Foo:
+    def __call__(self):
+        print 'called'
+
 def main():
-    name = sys.argv[1]
-    if name == 'David':
-        print repeat( name, False) + '!!!'
-    else:
-        print repeat( 'yay', True)
+    foo_instance =  Foo()
+    foo_instance()
 
 if __name__ == '__main__':
     main()
